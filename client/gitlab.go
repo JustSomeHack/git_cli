bddbdddbddc0ccd45059caad19f0febcb01fd10d
DeYung Le <deyung@gmail.com>
2019-08-30 02:47:27 -0500

    moved print functions

--- client/gitlab.go
+++ client/gitlab.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/JustSomeHack/git_cli/models"
 	"github.com/JustSomeHack/git_cli/models/gitlab"
+	"github.com/ryanuber/columnize"
 )
 
 // GitLab interface for client
@@ -15,6 +16,10 @@ type GitLab interface {
 	GetMergeRequests(projectID int) ([]gitlab.MergeRequest, error)
 	GetProjects() ([]gitlab.Project, error)
 	GetUsers() ([]gitlab.User, error)
+	PrintMergeRequests()
+	PrintProjects()
+	PrintUsers()
+	getProject(projects []gitlab.Project, projectID int) *gitlab.Project
 }
 
 type gitLab struct {
@@ -96,4 +101,65 @@ func (g *gitLab) GetUsers() ([]gitlab.User, error) {
 		log.Fatal(err)
 	}
 	return users, nil
+}
+
+func (g *gitLab) PrintMergeRequests() {
+	projects, err := g.GetProjects()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	requests, err := g.GetMergeRequests(-1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	requestsOutput := make([]string, 0)
+	requestsOutput = append(requestsOutput, "Request ID | Target Project | Source Project | Title | Description")
+	for _, request := range requests {
+		source := g.getProject(projects, request.SourceProjectID)
+		target := g.getProject(projects, request.TargetProjectID)
+		requestLine := fmt.Sprintf("%d | %s | %s | %s | %s", request.IID, target.PathWithNamespace, source.PathWithNamespace, request.Title, request.Description)
+		requestsOutput = append(requestsOutput, requestLine)
+	}
+	fmt.Println(columnize.SimpleFormat(requestsOutput))
+}
+
+func (g *gitLab) PrintProjects() {
+	projects, err := g.GetProjects()
+	if err != nil {
+		log.Fatal(err)
+	}
+	projectsOutput := make([]string, 0)
+	projectsOutput = append(projectsOutput, "Project ID | Path | Description")
+	for _, project := range projects {
+		projectLine := fmt.Sprintf("%d | %s | %s", project.ID, project.PathWithNamespace, project.Description)
+		projectsOutput = append(projectsOutput, projectLine)
+	}
+	fmt.Println(columnize.SimpleFormat(projectsOutput))
+}
+
+func (g *gitLab) PrintUsers() {
+	users, err := g.GetUsers()
+	if err != nil {
+		log.Fatal(err)
+	}
+	usersOutput := make([]string, 0)
+	usersOutput = append(usersOutput, "User ID | Name | Username | Status")
+	for _, user := range users {
+		if user.UserName == "ghost" || user.UserName == "root" {
+			continue
+		}
+		userLine := fmt.Sprintf("%d | %s | %s | %s", user.ID, user.Name, user.UserName, user.State)
+		usersOutput = append(usersOutput, userLine)
+	}
+	fmt.Println(columnize.SimpleFormat(usersOutput))
+}
+
+func (g *gitLab) getProject(projects []gitlab.Project, projectID int) *gitlab.Project {
+	for _, project := range projects {
+		if project.ID == projectID {
+			return &project
+		}
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/JustSomeHack/git_cli/client"
-	models "github.com/JustSomeHack/git_cli/models/gitlab"
-	"github.com/ryanuber/columnize"
 )
 
 var version string
@@ -33,55 +30,11 @@ func main() {
 
 	gitlab := client.NewGitLab(gitURL, apiKey)
 
-	users, err := gitlab.GetUsers()
-	if err != nil {
-		log.Fatal(err)
-	}
-	usersOutput := make([]string, 0)
-	usersOutput = append(usersOutput, "User ID | Name | Username | Status")
-	for _, user := range users {
-		if user.UserName == "ghost" || user.UserName == "root" {
-			continue
-		}
-		userLine := fmt.Sprintf("%d | %s | %s | %s", user.ID, user.Name, user.UserName, user.State)
-		usersOutput = append(usersOutput, userLine)
-	}
-	fmt.Println(columnize.SimpleFormat(usersOutput))
+	gitlab.PrintUsers()
 
-	projects, err := gitlab.GetProjects()
-	if err != nil {
-		log.Fatal(err)
-	}
-	projectsOutput := make([]string, 0)
-	projectsOutput = append(projectsOutput, "Project ID | Path | Description")
-	for _, project := range projects {
-		projectLine := fmt.Sprintf("%d | %s | %s", project.ID, project.PathWithNamespace, project.Description)
-		projectsOutput = append(projectsOutput, projectLine)
-	}
-	fmt.Println(columnize.SimpleFormat(projectsOutput))
+	gitlab.PrintProjects()
 
-	requests, err := gitlab.GetMergeRequests(-1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	requestsOutput := make([]string, 0)
-	requestsOutput = append(requestsOutput, "Request ID | Target Project | Source Project | Title | Description")
-	for _, request := range requests {
-		source := getProject(projects, request.SourceProjectID)
-		target := getProject(projects, request.TargetProjectID)
-		requestLine := fmt.Sprintf("%d | %s | %s | %s | %s", request.IID, target.PathWithNamespace, source.PathWithNamespace, request.Title, request.Description)
-		requestsOutput = append(requestsOutput, requestLine)
-	}
-	fmt.Println(columnize.SimpleFormat(requestsOutput))
+	gitlab.PrintMergeRequests()
 
 	gitlab.CreateMergeRequest(targetProject, sourceProject, "title", "description")
-}
-
-func getProject(projects []models.Project, id int) *models.Project {
-	for _, project := range projects {
-		if project.ID == id {
-			return &project
-		}
-	}
-	return nil
 }
